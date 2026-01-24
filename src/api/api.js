6069234ba33ffad1e25ef99b88599ddd367a67f7
SonagaraSandip <104724019+SonagaraSandip@users.noreply.github.com>
2026-01-24 12:51:36 +0530

    nothing

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://classis-backend.onrender.com/api"
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://classis-backend.onrender.com/api"
+  // baseURL: "http://localhost:5000/api",
 });
 
 //attack token automatically to every request
